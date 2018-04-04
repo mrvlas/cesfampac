04b21c3ef703f784ab21d921891adbfdcbec85fa
Vladimir Guajardo <VladimirGuajardo@MacBook-Pro-de-Vladimir.local>
2018-04-04 13:39:28 -0300

    estructura ok, tabs llamadas ok

--- src/data/menuprincipal.data.ts
+++ src/data/menuprincipal.data.ts
@@ -3,38 +3,28 @@
 export const MENU = [
     {
       img:'assets/imgs/cesfam.png',
-      nombre: 'CESFAM',
-      titulo: 'CESFAM Calle Larga'
+      nombre: 'CESFAM PAC',
+      titulo: 'Centro de Salud Familiar Pedro Aguirre Cerda de Calle Larga'
     },
     {
-      img:'assets/imgs/rey.jpg',
-      nombre: 'Rey',
-      titulo: 'Recolectora'
+      img:'assets/imgs/pide_tu_hora.png',
+      nombre: 'Pide tú hora',
+      titulo: 'Numeros telefonicos para solicitar hora'
     },
     {
-      img:'assets/imgs/finn.jpg',
-      nombre: 'Finn',
-      titulo: 'Piloto'
+      img:'assets/imgs/alimentacion_saludable.png',
+      nombre: 'Alimnetación saludable',
+      titulo: 'Energía y los nutrientes que necesitas '
     },
     {
-      img:'assets/imgs/leia.jpg',
-      nombre: 'Leia',
-      titulo: 'Princesa'
+      img:'assets/imgs/ejercicios.png',
+      nombre: 'Ejercicios',
+      titulo: 'Ejercicios saludables y divertidos'
     },
     {
-      img:'assets/imgs/leia.jpg',
-      nombre: 'Leia',
-      titulo: 'Princesa'
-    },
-    {
-      img:'assets/imgs/leia.jpg',
-      nombre: 'Leia',
-      titulo: 'Princesa'
-    },
-    {
-      img:'assets/imgs/leia.jpg',
-      nombre: 'Leia',
-      titulo: 'Princesa'
+      img:'assets/imgs/preguntas.png',
+      nombre: 'Preguntas y respuestas',
+      titulo: 'Todo lo que necesitas saber del CESFAM'
     },
   ];
   
