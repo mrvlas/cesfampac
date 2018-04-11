e83d43d2fd444edd3062a74a86432dd897f03f55
Vladimir Guajardo <VladimirGuajardo@MacBook-Pro-de-Vladimir.local>
2018-04-11 13:39:14 -0300

    programando el call number en el menu cesfam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { CallNumber } from '@ionic-native/call-number';
 
 import { MyApp } from './app.component';
 //import { HomePage } from '../pages/home/home';
@@ -44,6 +45,7 @@ import {DetallePage,
   providers: [
     StatusBar,
     SplashScreen,
+    CallNumber,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/tabs-llamar/tabs-llamar.ts
+++ src/pages/tabs-llamar/tabs-llamar.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the TabsLlamarPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { CallNumber } from '@ionic-native/call-number';
 
 @IonicPage()
 @Component({
@@ -15,11 +9,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TabsLlamarPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private callNumber: CallNumber) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TabsLlamarPage');
+  }
+
+  llamarCesfam(){
+    console.log("Dentro de la funcion llamar");
+    this.callNumber.callNumber("342436200", true)
+    .then(res => console.log('Launched dialer!', res))
+    .catch(err => console.log('Error launching dialer', err));
   }
 
 }
